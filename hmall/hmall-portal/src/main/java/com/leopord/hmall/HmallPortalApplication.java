d34b1dce4257ec6d1da6398d71218fca3fa34037
unknown <huangchenhong@360buyad.local>
2020-11-13 11:12:14 +0800

    init web

---
+++ hmall/hmall-portal/src/main/java/com/leopord/hmall/HmallPortalApplication.java
@@ -0,0 +1,13 @@
+package com.leopord.hmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmallPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmallPortalApplication.class, args);
+	}
+
+}
---
+++ hmall/hmall-portal/src/test/java/com/leopord/hmall/HmallPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leopord.hmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmallPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
