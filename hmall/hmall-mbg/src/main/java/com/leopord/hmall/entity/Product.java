b1c8ef61e65dc4fab30eb6209f6beccc2dba8486
unknown <huangchenhong@360buyad.local>
2020-11-13 20:02:44 +0800

    mapper

---
+++ hmall/hmall-mbg/src/main/java/com/leopord/hmall/entity/Product.java
@@ -0,0 +1,84 @@
+package com.leopord.hmall.entity;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+    private Integer id;
+
+    private String title;
+
+    private Double price;
+
+    private Integer sales;
+
+    private Integer promotionId;
+
+    private Double promotionPrice;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getSales() {
+        return sales;
+    }
+
+    public void setSales(Integer sales) {
+        this.sales = sales;
+    }
+
+    public Integer getPromotionId() {
+        return promotionId;
+    }
+
+    public void setPromotionId(Integer promotionId) {
+        this.promotionId = promotionId;
+    }
+
+    public Double getPromotionPrice() {
+        return promotionPrice;
+    }
+
+    public void setPromotionPrice(Double promotionPrice) {
+        this.promotionPrice = promotionPrice;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", title=").append(title);
+        sb.append(", price=").append(price);
+        sb.append(", sales=").append(sales);
+        sb.append(", promotionId=").append(promotionId);
+        sb.append(", promotionPrice=").append(promotionPrice);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
