9cc1f07be12a65d7d1ace2b07bd5cdebbe0c456e
Leopord7 <1160913868@qq.com>
2020-11-14 20:25:44 +0800

    add ignore

--- hmall/hmall-portal/src/main/java/com/leopord/hmall/HmallPortalApplication.java
+++ hmall/hmall-portal/src/main/java/com/leopord/hmall/HmallPortalApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HmallPortalApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HmallPortalApplication.class, args);
 	}
